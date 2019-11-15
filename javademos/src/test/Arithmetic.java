f958079c1c9c80d0a4b41f4cddd23caaabf5c7a1
abirami1607 <57611482+abirami1607@users.noreply.github.com>
2019-11-15 09:49:32 +0530

    PRIORITY

---
+++ javademos/src/test/Arithmetic.java
@@ -0,0 +1,9 @@
+package test;
+import java.util.Scanner;
+
+public class Arithmetic {
+	public static void main(String[] args) {
+		
+	}
+
+}
--- javademos/src/test/Friend.java
+++ javademos/src/test/Friend.java
@@ -1,25 +1,26 @@
 package test;
-import java
-import java.util.Scanner; util.Scanner;
+import java.util.Scanner;
 public class Friend 
 {
 	public static void main(String[] args)
 	{
-	double deposit;
-	int rate,threshold,temp;
+	double deposit,temp=0;
+	int rate,threshold;
 	Scanner sc1=new Scanner(System.in);
 	System.out.print("Enter the deposit amount:");
 	deposit=sc1.nextDouble();
 	System.out.print("Enter the rate of interest:");
 	rate=sc1.nextInt();
-	System.out.print("Enter the threshold value");
-	theshold=sc1.nextInt();
-	int count==0;
+	System.out.print("Enter the threshold value:");
+	threshold=sc1.nextInt();
+	int count=0;
 	while(deposit<threshold)
 	{
-		temp=((deposit/100)*rate)
-				count++
+		temp=((deposit/100)*rate);
+		deposit=deposit+temp;
+				count++;
 	}
+	System.out.print("number of years:"+count);
 	
 	
 		
